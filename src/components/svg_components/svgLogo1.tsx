a10e6ed2e4532cd4d9d5f206ae837599f48d30e0
IT-Gleb <139981680+IT-Gleb@users.noreply.github.com>
2025-03-12 06:07:44 +0300

    commit 12-03-2025 7

--- src/components/svg_components/svgLogo1.tsx
+++ src/components/svg_components/svgLogo1.tsx
@@ -4,7 +4,7 @@ export const SvgLogo1 = () => {
   return (
     <svg
       version="1.1"
-      id="Frame_0"
+      id="Frame_2"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       width="100%"
