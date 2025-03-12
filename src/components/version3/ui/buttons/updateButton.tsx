1bf88eb6fa6834f09a46a57b27368f09c1f9e540
IT-Gleb <139981680+IT-Gleb@users.noreply.github.com>
2025-03-12 05:42:48 +0300

    commit 12-03-2025 2

--- src/components/version3/ui/buttons/updateButton.tsx
+++ src/components/version3/ui/buttons/updateButton.tsx
@@ -14,9 +14,9 @@ export const UpdateButton: FC<TUpdateProps> = ({ title, click }) => {
       onClick={() => click()}
       className="min-w-[60px] p-1 rounded-md bg-green-600 text-green-50 active:scale-90 place-content-center group"
     >
-      <div className="w-[22px] h-[22px] mx-auto group-active:text-yellow-300">
+      <span className="block w-[22px] h-[22px] mx-auto group-active:text-yellow-300">
         <SvgLoading />
-      </div>
+      </span>
     </button>
   );
 };
