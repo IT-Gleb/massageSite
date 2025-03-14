7603e7aa4bec8bde712ad1a6869c7df957ffc254
IT-Gleb <139981680+IT-Gleb@users.noreply.github.com>
2025-03-14 19:13:07 +0300

    proba 2

--- src/components/version3/layout/footerLayout.tsx
+++ src/components/version3/layout/footerLayout.tsx
@@ -1,4 +1,4 @@
-import React, { FC, Suspense } from "react";
+import React, { FC } from "react";
 import { ContainerContent } from "../content/containerContent";
 import { InfoObj } from "@/utils/info";
 import { getNowYear } from "@/utils/functions";
@@ -6,17 +6,15 @@ import { getNowYear } from "@/utils/functions";
 export const FooterLayout: FC = () => {
   return (
     <footer className="text-green-50">
-      <Suspense>
-        <ContainerContent backgroundClass="bg-green-700">
-          <div className="pb-20 flex flex-wrap items-center gap-2 justify-evenly text-[4vw]/[4vw] md:text-[1.6vw]/[1.7vw] lg:text-[1vw]/[1.1vw]">
-            <a href={`tel:+${InfoObj.phoneReg}`} className="font-oswald">
-              тел.: {InfoObj.phone_mask}
-            </a>
-            <p className="p-2">{InfoObj.FullAdress}</p>
-            <p>&copy; 2001-{getNowYear()}г.</p>
-          </div>
-        </ContainerContent>
-      </Suspense>
+      <ContainerContent backgroundClass="bg-green-700">
+        <div className="pb-20 flex flex-wrap items-center gap-2 justify-evenly text-[4vw]/[4vw] md:text-[1.6vw]/[1.7vw] lg:text-[1vw]/[1.1vw]">
+          <a href={`tel:+${InfoObj.phoneReg}`} className="font-oswald">
+            тел.: {InfoObj.phone_mask}
+          </a>
+          <p className="p-2">{InfoObj.FullAdress}</p>
+          <p>&copy; 2001-{getNowYear()}г.</p>
+        </div>
+      </ContainerContent>
     </footer>
   );
 };
