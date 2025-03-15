680de1717ae3935f471580fe03ff4abdf2adf72c
IT-Gleb <139981680+IT-Gleb@users.noreply.github.com>
2025-03-15 08:38:28 +0300

    commit 15-03-2025 3

--- src/components/version3/content/contentHowRecord.tsx
+++ src/components/version3/content/contentHowRecord.tsx
@@ -9,15 +9,15 @@ import { useUserOS } from "@/hooks/getUserPlatform";
 import { TextTitle } from "../ui/buttons/heading/textTitle";
 
 export const ContentHowRecord: FC = () => {
-  const aimoRef = useRef<HTMLDivElement>(null);
-  const inView = useInView(aimoRef, { amount: 0.5, once: false });
+  const animoRef = useRef<HTMLDivElement>(null);
+  const inView = useInView(animoRef, { amount: 0.5, once: false });
   const preferReducedMotion = useReducedMotion();
   const [canAnimation, setCanAnimation] = useState<boolean>(
     preferReducedMotion !== null
       ? preferReducedMotion
         ? preferReducedMotion
         : false
-      : false
+      : true
   );
 
   const { userOS } = useUserOS();
@@ -25,7 +25,7 @@ export const ContentHowRecord: FC = () => {
   if (!["Windows", "Android", "Linux"].includes(userOS)) {
     return (
       <ContainerContent backgroundClass="bg-sky-50/50">
-        <h2>{userOS}</h2>
+        <TextTitle title={userOS} />
         <div className="w-full h-[2vh] lg:h-[5vh] bg-[url('/images/svg/back.svg')] mt-5"></div>
       </ContainerContent>
     );
@@ -36,13 +36,13 @@ export const ContentHowRecord: FC = () => {
     <ContainerContent backgroundClass="bg-sky-50/50">
       <div className="flex flex-col min-h-[35vh] xl:h-screen">
         <div className="flex items-center gap-x-2 justify-between">
-          <TextTitle title="Как помочь себе" />
+          <TextTitle title={`Как помочь себе (${userOS})`} />
           <AnimoButton
             title={canAnimation ? "Остановить" : "Просмотреть"}
             click={() => setCanAnimation(!canAnimation)}
           />
         </div>
-        <div ref={aimoRef} className="w-full mt-5 flex-auto">
+        <div ref={animoRef} className="w-full mt-[4rem] flex-auto">
           <SvgInfoGraphics1 doAnimate={inView} canAnimate={canAnimation} />
         </div>
       </div>
