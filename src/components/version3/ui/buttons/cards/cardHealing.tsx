9fdafb897e5d9787ca5de8915b6c11556442dae8
IT-Gleb <139981680+IT-Gleb@users.noreply.github.com>
2025-03-19 19:23:48 +0300

    commit 19-03-2025 3

--- src/components/version3/ui/buttons/cards/cardHealing.tsx
+++ src/components/version3/ui/buttons/cards/cardHealing.tsx
@@ -17,18 +17,18 @@ export const CardHealing: FC<TCardHealingProps> = ({
   return (
     <div
       ref={thisRef}
-      className={`relative min-h-[5vh] pl-5 lg:pl-8 py-2 mt-1 place-content-start transition-all ${
+      className={`relative min-h-[8vh] pl-5 lg:pl-8 py-2 mt-1 place-content-center transition-all ${
         isSelected
           ? "bg-[linear-gradient(to_right,theme(colors.green.400),theme(colors.emerald.200),theme(colors.transparent))] text-indigo-950 font-semibold font-roboto"
           : "bg-[linear-gradient(to_right,theme(colors.green.200),theme(colors.transparent))] font-verdana"
       } `}
     >
       <div
-        className={`absolute w-[28px] h-[28px] md:w-[34px] md:h-[34px] lg:w-[36px] lg:h-[36px] left-0 translate-x-[-50%] top-[50%] translate-y-[-50%]
+        className={`absolute w-[28px] h-[28px] md:w-[34px] md:h-[34px] lg:w-[36px] lg:h-[36px] left-0 translate-x-[-50%] top-0 border-2
            text-[1.3em]/[1.3em] md:text-[1em]/[1em] lg:text-[0.9em]/[0.9em] font-oswald place-content-center ${
              isSelected
-               ? "bg-sky-500 text-yellow-100"
-               : "bg-rose-300 text-green-50"
+               ? "bg-yellow-200 text-slate-800 border-green-600"
+               : "bg-green-600 text-green-50 border-green-50"
            } rounded-full text-center overflow-hidden`}
       >
         {zeroToNumber(Num)}
